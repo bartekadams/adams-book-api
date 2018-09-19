30f903f76130dd8bdace0bfe92813014d9d104d7
Bartosz Adamski <bartek.adams94@gmail.com>
2018-09-19 22:39:38 +0200

    Books CRUD operations.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,5 +4,96 @@ class Api::V1::BooksController < ApplicationController
             data: Book.limit(10)
         }
     end
+
+    def show
+        book = Book.find(params[:id])
+        if book
+            render json: {
+                status: "SUCCESS",
+                message: "Book found",
+                data: book
+            }, status: :ok
+        else
+            book_not_found
+        end
+    end
+
+    def create
+        book = current_user.books.new(book_params)
+        if book.save
+            render json: {
+                status: "SUCCESS",
+                message: "Book created",
+                data: book
+            }, status: :created
+        else
+            render json: {
+                status: "ERROR",
+                message: "Book not created",
+                data: book.errors
+            }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        book = Book.find(params[:id])
+        if book
+            unless book.owner_id == current_user.id
+                book_not_yours
+            else
+                if book.update(book_params)
+                    render json: {
+                        status: "SUCCESS",
+                        message: "Book updated",
+                        data: book
+                    }, status: :ok
+                else
+                    render json: {
+                        status: "ERROR",
+                        message: "Book not updated",
+                        data: book.errors
+                    }, status: :unprocessable_entity
+                end
+            end
+        else
+            book_not_found
+        end
+    end
+
+    def destroy
+        book = Book.find(params[:id])
+        if book
+            unless book.owner_id == current_user.id
+                book_not_yours
+            else
+                render json: {
+                    status: "SUCCESS",
+                    message: "Book deleted",
+                    data: book.destroy
+                }, status: :ok
+            end
+        else
+            book_not_found
+        end
+    end
+
+    private
+    def book_params
+        params.require(:book).permit(:name)
+    end
+
+    def book_not_found
+        render json: {
+            status: "ERROR",
+            message: "Book not found"
+        }, status: :not_found
+    end
+
+    def book_not_yours
+        render json: {
+            status: "ERROR",
+            message: "Book does not belong to you"
+        }, status: :forbidden
+    end
     
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
     belongs_to :owner, class_name: "User", foreign_key: 'owner_id'
+
+    validates :name, presence: true
 end
