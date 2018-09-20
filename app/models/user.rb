085ba7f5af6418dda633838d7c71df7e4c8386e5
Bartosz Adamski <bartek.adams94@gmail.com>
2018-09-20 19:52:02 +0200

    More associations to get borrowed books, borrowers

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@ class User < ApplicationRecord
 
     # has many books and in the books table this user is referenced by owner_id
     has_many :books, foreign_key: "owner_id", inverse_of: 'owner'
-    has_many :loans, foreign_key: "borrower_id", inverse_of: "borrower"
-    #has_many :borrowed_books, class_name: "Book", foreign_key: "borrower_id", inverse_of: "borrower"
+
+    # has many loans as book owner
+    has_many :loans_as_owner, through: :books, source: :loans
+
+    # has many borrowers, borrower is user referenced from loans table
+    has_many :borrowers, through: :loans_as_owner, source: :borrower
+
+    # has many loans as book borrower
+    has_many :loans_as_borrower, class_name: "Loan", foreign_key: "borrower_id", inverse_of: "borrower"
+
+    # has many borrowed books
+    has_many :borrowed_books, through: :loans_as_borrower, source: :book
 end
